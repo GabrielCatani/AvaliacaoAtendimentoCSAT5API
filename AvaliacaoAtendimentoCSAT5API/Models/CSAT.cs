6d6b87300db2d990153a04890a37ea1e3fa13a88
Gabriel Catani <gabrielm.catani@gmail.com>
2023-07-24 15:04:14 -0300

    Adds  Entity Model. Adds  file, with MongoDB credentials and DB access config

---
+++ AvaliacaoAtendimentoCSAT5API/Models/CSAT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AvaliacaoAtendimentoCSAT5API.Models
+{
+	public class CSAT
+	{
+	
+		public Guid Id { get; set; }
+		public int Score { get; set; }
+		public string? Comment { get; set; }
+		public bool problemSolved { get; set; }
+		public string? Email { get; set; }
+		public DateTime TimeStamp { get; set; }
+	}
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ﻿# Avaliação de Atendimento CSAT5
-    O CSAT 5 consiste em uma iniciativa da XP para que considerar como promotor
+
+O CSAT 5 consiste em uma iniciativa da XP para que considerar como promotor
 apenas a nota 5 e como detrator as notas 1, 2 e 3.
