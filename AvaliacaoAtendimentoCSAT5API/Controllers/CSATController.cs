2634f03f91651988a1d685b24e48a3f5587ebdbe
Gabriel Catani <gabrielm.catani@gmail.com>
2023-07-25 09:42:41 -0300

    Adds `CSATController`. Adds `CSATService` to persist `CSAT` model.

---
+++ AvaliacaoAtendimentoCSAT5API/Controllers/CSATController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliacaoAtendimentoCSAT5API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CSATController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        [HttpGet("{id}")]
+        public void IndexReceive(string id)
+        {
+            Console.WriteLine(id);
+        }
+    }
+}
+
