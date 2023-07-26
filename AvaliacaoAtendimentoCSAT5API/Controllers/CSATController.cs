45b7d2595b2950ad565e72f23a7d67760b197df2
Gabriel Catani <gabrielm.catani@gmail.com>
2023-07-26 11:57:34 -0300

    Updates `CSATController` with route to create new CSAT

--- AvaliacaoAtendimentoCSAT5API/Controllers/CSATController.cs
+++ AvaliacaoAtendimentoCSAT5API/Controllers/CSATController.cs
@@ -4,27 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AvaliacaoAtendimentoCSAT5API.Models;
+using AvaliacaoAtendimentoCSAT5API.Services;
 
 namespace AvaliacaoAtendimentoCSAT5API.Controllers
 {
+  
     [ApiController]
     [Route("api/[controller]")]
-    public class CSATController : Controller
+    public class CSATController : ControllerBase
     {
+        private readonly CSATService _csatService;
+
+        public CSATController(CSATService csatService)
+        {
+            _csatService = csatService;
+        }
+
         [HttpGet]
         public string Index()
         {
             return "Hello World";
         }
 
-        [HttpPost("/createCSAT")]
-        public IActionResult PostCSAT(CSAT newCSAT)
+        [HttpPost("createCSAT")]
+        public async Task<IActionResult> PostCSAT(CSAT newCSAT)
         {
             if (newCSAT.Score < 1 || newCSAT.Score > 5) {
                 return BadRequest("Invalid CSAT Score");
             }
 
             //persist CSAT
+            await _csatService.CreateAsync(newCSAT);
             
             return Ok(newCSAT.Id);
         }
